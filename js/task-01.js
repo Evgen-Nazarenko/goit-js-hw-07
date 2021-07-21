64d795918567158cce25350ec149622604a47454
evgen nazarenko <nazarenkoevgen99@gmail.com>
2021-07-21 10:44:44 +0300

    homework-7

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll(".item");
+console.log(`В списку ${items.length} категорії.`);
+items.forEach(function (item) {
+  console.log(`Категорія: ${item.firstElementChild.textContent}`);
+  let ulList = item.children[1],
+    ulListChildrens = ulList.children;
+  console.log(`Кількість елементів: ${ulListChildrens.length}`);
+});
